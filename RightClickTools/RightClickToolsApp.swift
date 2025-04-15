f085635aa34e1992da5bc8df21e0698b7e082dfe
wangdesheng_sc <wangdesheng_sc@extern.faw-vw.com>
2025-04-15 15:04:50 +0800

    Initial Commit

---
+++ RightClickTools/RightClickToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightClickToolsApp.swift
+//  RightClickTools
+//
+//  Created by kylin on 2025/4/15.
+//
+
+import SwiftUI
+
+@main
+struct RightClickToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RightClickToolsTests/RightClickToolsTests.swift
@@ -0,0 +1,17 @@
+//
+//  RightClickToolsTests.swift
+//  RightClickToolsTests
+//
+//  Created by kylin on 2025/4/15.
+//
+
+import Testing
+@testable import RightClickTools
+
+struct RightClickToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
